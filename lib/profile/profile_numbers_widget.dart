322997dd394e2c3d92494dc1dad6fbf05bd52515
nathanbowness <nathanbowness@gmail.com>
2022-08-14 20:16:36 -0400

    Update to start adding profile numbers and information about them

---
+++ lib/profile/profile_numbers_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ProfileNumbersWidget extends StatelessWidget {
+  const ProfileNumbersWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        profileNumbersButton(context, "4.8", "Stars"),
+        profileNumbersButton(context, "58", "Appointments"),
+      ],
+    );
+  }
+
+  Widget profileNumbersButton(BuildContext context, String value, String text) {
+    return MaterialButton(
+      onPressed: () {},
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(value),
+          const SizedBox(
+            height: 2,
+          ),
+          Text(text)
+        ],
+      ),
+    );
+  }
+}
--- lib/profile/profile_page.dart
+++ lib/profile/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_profiles/model/user.dart';
+import 'package:flutter_profiles/profile/profile_numbers_widget.dart';
 import 'package:flutter_profiles/profile/profile_user_widget.dart';
 import 'package:flutter_profiles/utils/user_preferences.dart';
 import 'package:flutter_profiles/widget/appbar_widget.dart';
@@ -22,9 +24,25 @@ class ProfilePageState extends State<ProfilePage> {
         physics: const BouncingScrollPhysics(),
         children: [
           const SizedBox(height: 24),
-          ProfileWidget(imagePath: testUser.imagePath, onClicked: () {})
+          ProfileWidget(imagePath: testUser.imagePath, onClicked: () {}),
+          const SizedBox(height: 24),
+          buildName(testUser),
+          ProfileNumbersWidget(context),
         ],
       ),
+    );
+  }
+
+  Widget buildName(User user) {
+    return Column(
+      children: [
+        Text(
+          user.name,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        const SizedBox(height: 4),
+        Text(user.email, style: const TextStyle(color: Colors.grey))
+      ],
     );
   }
 }
